774e552b336d70034aa65c7c677bcf0b8a303dc8
Rebecca McIntosh and senya webster <rebecca@>
2017-08-07 13:43:23 -0700

    first conversion liters to gallons

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+var gallon = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("enter gallons:"));
+var number2 = 3.78541;
+
+alert(gallon(number1, number2));
