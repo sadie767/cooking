4d72a29e9f8db1dcaa2073e0597fc63fe818e719
Rebecca McIntosh and senya webster <senya@>
2017-08-07 14:02:29 -0700

    add ml and ounces conversion

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,23 @@ var number1 = parseInt(prompt("enter gallons:"));
 var number2 = 3.78541;
 
 alert(gallon(number1, number2));
+
+
+var liter = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("enter liters:"));
+var number2 = 33.814;
+
+alert(liter(number1, number2));
+
+
+var liter = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("enter liters:"));
+var number2 = 1000;
+
+alert(liter(number1, number2));
